7991b43c9221c80090adc218b4ca839aeece2d71
Beekasha <brian.j.kleiner@gmail.com>
2020-07-29 01:59:03 -0400

    removed review tester / delete now works

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,2 @@
 class Movie < ApplicationRecord
-    has_many :reviews, dependent: :destroy
-    accepts_nested_attributes_for :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-
-  resources :reviews
   resources :movies
 end
